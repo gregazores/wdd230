4bce767f8509326780177a1434b621d6389cf415
gregclar2019 <daddygreggy09@gmail.com>
2022-10-09 10:19:25 -0400

    made corrections based on the validation

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,4 +1,23 @@
+//Let's get the date first
 let d = new Date()
+//format the date into a UK format date
+let fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(d);
+document.querySelector(".current-date p").innerHTML = fulldate
+
+//Resposive Menu Scripts
+let burgerMenu = document.querySelector("#burger-icon")
+let closerIcon = document.querySelector("#close-icon")
+burgerMenu.addEventListener("click", addActive)
+closerIcon.addEventListener("click", addActive)
+function addActive(){
+    document.querySelector("#responsive-nav").classList.toggle("active")
+}
+
+
+//Footer Meta scripts
+//Will change content depending on the size of the window
 let last_mod = document.lastModified
 let footer_text = `&copy; ${d.getFullYear()} Ozamiz Chamber | <strong>Jeevee Greg Azores</strong> | WDD 230 Project | Last Modification: ${last_mod}`
 let footer_text2 = `&copy; ${d.getFullYear()} Ozamiz Chamber<br><strong>Jeevee Greg Azores</strong><br>WDD 230 Project<br>Last Modification: ${last_mod}`
@@ -17,49 +36,4 @@ function checkSize() {
 }
 
 
-
-
-let burgerMenu = document.querySelector("#burger-icon")
-let closerIcon = document.querySelector("#close-icon")
-burgerMenu.addEventListener("click", addActive)
-closerIcon.addEventListener("click", addActive)
-function addActive(){
-    document.querySelector("#responsive-nav").classList.toggle("active")
-}
-
-
-
-/*
-
-
-let footer_text2 = `&copy; ${d.getFullYear()} Ozamiz Chamber<br><strong>Jeevee Greg Azores</strong><br>WDD 230 Project<br>Last Modification: ${last_mod}`
-let footer_copyright = document.getElementById("footer-copyright")
-
-let size = 1080
-
-window.addEventListener("resize", checkSize)
-function checkSize() {
-    size = footer_copyright.offsetWidth 
-    if (size < 500)  {
-        alert("less 500")
-    } 
-}
-
-window.addEventListener("resize", checkSize)
-function checkSize() {
-    if (footer_copyright.offsetWidth < 500) {
-        footer_copyright.innerHTML = footer_text2    
-}
-
-document.querySelector(".footer-meta-info").addEventListener("resize", checkSize)
-function checkSize() {
-    if (footer_copyright.offsetWidth < 500) {
-        footer_copyright.innerHTML = footer_text2
-    } else {
-        
-    }
-    
-}
-
-*/
 

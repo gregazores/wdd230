0785f4e742d13e8291f66f15ca7afc24acc50bea
gregclar2019 <daddygreggy09@gmail.com>
2022-10-28 01:49:40 -0400

    New Css

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -12,7 +12,7 @@ function computeWindChill() {
     if (temperature <= 50 && windSpeed > 30) {
         let chill = 35.74 + (0.6215*temperature) - (35.75*(windSpeed**.16)) + (.4275*temperature*(windSpeed**.16))
         windChill = chill
-        chillHTML.innerHTML = `${Math.round(windChill)}&#8457`
+        chillHTML.innerHTML = `${Math.round(windChill)}&#8457;`
     } else {
         chillHTML.innerHTML = `N/A` 
     }

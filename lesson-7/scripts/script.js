b732f6c114aa66cfb9a04513788fa85832f32a7c
gregclar2019 <daddygreggy09@gmail.com>
2022-10-30 10:26:47 -0400

    new lazy loading scrip and css

--- lesson-7/scripts/script.js
+++ lesson-7/scripts/script.js
@@ -4,10 +4,40 @@ let d = new Date()
 let fulldateUK = new Intl.DateTimeFormat("en-UK", {
 	dateStyle: "full"
 }).format(d);
-document.querySelector(".current-date p").innerHTML = fulldateUK
 
-//Resposive Menu Scripts
+//Lazy Loading Script
+const images = document.querySelectorAll('[data-src]')
 
+function preloadImage(img) {
+    const src = img.getAttribute('data-src')
+    if(!src) {
+        return
+    }
+
+    img.src = src
+    img.classList.add('animated')
+}
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px -500px 0px'
+}
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target)
+            imgObserver.unobserve(entry.target)
+        }
+    })
+
+}, imgOptions)
+
+images.forEach(image => {
+    imgObserver.observe(image)
+})
 
 
 

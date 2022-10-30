606a4968e763cb0b81f605d3babd502b8e69b01e
gregclar2019 <daddygreggy09@gmail.com>
2022-10-30 00:34:38 -0400

    lazy loading page

---
+++ lesson-7/scripts/script.js
@@ -0,0 +1,36 @@
+//Let's get the date first
+let d = new Date()
+//format the date into a UK format date
+let fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(d);
+document.querySelector(".current-date p").innerHTML = fulldateUK
+
+//Resposive Menu Scripts
+
+
+
+
+
+
+//Footer Meta scripts
+//Will change content depending on the size of the window
+let last_mod = document.lastModified
+let footer_text = `&copy; ${d.getFullYear()} Ozamiz Chamber | <strong>Jeevee Greg Azores</strong> | WDD 230 Project | Last Modification: ${last_mod}`
+let footer_text2 = `&copy; ${d.getFullYear()} Ozamiz Chamber<br><strong>Jeevee Greg Azores</strong><br>WDD 230 Project<br>Last Modification: ${last_mod}`
+let footer_copyright = document.getElementById("footer-copyright")
+footer_copyright.innerHTML = footer_text
+let size = 1080
+
+window.addEventListener("resize", checkSize)
+function checkSize() {
+    size = footer_copyright.offsetWidth 
+    if (size < 500)  {
+        footer_copyright.innerHTML = footer_text2
+    } else {
+        footer_copyright.innerHTML = footer_text
+    }
+}
+
+
+
---
+++ lesson-7/scripts/windchill.js
@@ -0,0 +1,22 @@
+let temperature = 35
+let windSpeed = 33
+let windChill = 0
+let tempHTML = document.querySelector("#temperature")
+let windHTML = document.querySelector("#wind-speed")
+let chillHTML = document.querySelector("#wind-chill")
+tempHTML.innerHTML = `${temperature}&#8457`
+windHTML.innerHTML = `${windSpeed}mi/h`
+chillHTML.innerHTML = `N/A`
+
+function computeWindChill() {
+    if (temperature <= 50 && windSpeed > 30) {
+        let chill = 35.74 + (0.6215*temperature) - (35.75*(windSpeed**.16)) + (.4275*temperature*(windSpeed**.16))
+        windChill = chill
+        chillHTML.innerHTML = `${Math.round(windChill)}&#8457;`
+    } else {
+        chillHTML.innerHTML = `N/A` 
+    }
+  
+}
+
+computeWindChill()

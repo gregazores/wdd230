8221f814def62634a06483aacf304ad13d9c7694
gregclar2019 <daddygreggy09@gmail.com>
2022-11-02 05:19:06 -0400

    discover css and lazy loading

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -22,6 +22,41 @@ window.addEventListener('load', (event) => {
     }
 });
 
+//Lazy Loading Script
+const images = document.querySelectorAll('[data-src]')
+
+function preloadImage(img) {
+    const src = img.getAttribute('data-src')
+    if(!src) {
+        return
+    }
+
+    img.src = src
+    img.classList.add('animated')
+}
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px -500px 0px'
+}
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target)
+            imgObserver.unobserve(entry.target)
+        }
+    })
+
+}, imgOptions)
+
+images.forEach(image => {
+    imgObserver.observe(image)
+})
+
+
 
 
 

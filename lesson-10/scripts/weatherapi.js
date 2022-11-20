afc06a17c3a3aff221df740f472e2e67edd7f659
gregclar2019 <daddygreggy09@gmail.com>
2022-11-19 22:42:42 -0500

    new open weather API

---
+++ lesson-10/scripts/weatherapi.js
@@ -0,0 +1,68 @@
+//Let's get the date first
+let d = new Date()
+//format the date into a UK format date
+let fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(d);
+
+// weather API script
+
+/*
+In your "weatherapi.js" file, first select all of the HTML elements 
+that will need to be modified/manipulated and assign them to const variables 
+(These will not change during in the coding of this activity).
+
+*/
+
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+/*
+Now create an "url" variable using const that stores the 'https://api.openweathermap.org/...' 
+URL as demonstrated in in the API documentation given.
+*/
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=1c04d13839472f4456588c22392bbf46';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        // displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+
+
+//Footer Meta scripts
+//Will change content depending on the size of the window
+let last_mod = document.lastModified
+let footer_text = `&copy; ${d.getFullYear()} Ozamiz Chamber | <strong>Jeevee Greg Azores</strong> | WDD 230 Project | Last Modification: ${last_mod}`
+let footer_text2 = `&copy; ${d.getFullYear()} Ozamiz Chamber<br><strong>Jeevee Greg Azores</strong><br>WDD 230 Project<br>Last Modification: ${last_mod}`
+let footer_copyright = document.getElementById("footer-copyright")
+footer_copyright.innerHTML = footer_text
+let size = 1080
+
+window.addEventListener("resize", checkSize)
+function checkSize() {
+    size = footer_copyright.offsetWidth 
+    if (size < 500)  {
+        footer_copyright.innerHTML = footer_text2
+    } else {
+        footer_copyright.innerHTML = footer_text
+    }
+}
+
+
+

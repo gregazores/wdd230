60fd1fdc1434e35532a80d5cd32f9b2c87381d41
gregclar2019 <daddygreggy09@gmail.com>
2022-11-21 05:32:17 -0500

    completed discover and home page

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -26,13 +26,32 @@ function displayCompanies(company) {
     </div>
 
     <div class="bottom-content">
-        <h3 class="common-header-three">${company.name}</h3>
-        <p class="business-categories">${company.category}</p>
-        <p class="business-address">📍 ${company.address}</p>
-        <p class="business-phone">&#128222; ${company.phone}</p>
+        <div>
+          <h3 class="common-header-three">${company.name}</h3>
+          <p class="business-categories">${company.category}</p>
+        </div>
+        <div>
+          <p class="business-address">📍 ${company.address}</p>
+          <p class="business-phone">&#128222; ${company.phone}</p>
+        </div>
         <a target="${company.website}" href="discover.html" class="generic-button-blue">Visit Site</a>
     </div>
     
     `
     document.querySelector('.business-directory-container').append(card);
 }
+
+//event listener to convert 
+const gridButton = document.querySelector('#grid')
+const listButton = document.querySelector('#list')
+gridButton.addEventListener('click', () => {
+  document.querySelector('.business-directory-container').classList.remove('grid-list')
+  gridButton.style.borderColor = '#303030'
+  listButton.style.borderColor = '#BFDBF7'
+})
+
+listButton.addEventListener('click', () => {
+  document.querySelector('.business-directory-container').classList.add('grid-list')
+  listButton.style.borderColor = '#303030'
+  gridButton.style.borderColor = '#BFDBF7'
+})

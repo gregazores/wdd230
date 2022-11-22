f6850ed20c4861928dd8333b43b35b9de06aa6e3
gregclar2019 <daddygreggy09@gmail.com>
2022-11-22 02:05:14 -0500

    fixed css bug in discover page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -41,7 +41,7 @@ function preloadImage(img) {
 
 const imgOptions = {
     threshold: 0,
-    rootMargin: '0px 0px -500px 0px'
+    rootMargin: '0px 0px 0px 0px'
 }
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {

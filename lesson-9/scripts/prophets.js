c55f629346db9c9269527e23661f5f3ec71d3529
gregclar2019 <daddygreggy09@gmail.com>
2022-11-22 03:30:54 -0500

    prohets style and layout

--- lesson-9/scripts/prophets.js
+++ lesson-9/scripts/prophets.js
@@ -1,3 +1,12 @@
+//Let's get the date first
+let d = new Date()
+//format the date into a UK format date
+let fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(d);
+
+
+
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 const cards = document.querySelector('.cards');
 
@@ -17,10 +26,14 @@ function displayProphets(prophet) {
     // Create elements to add to the document
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
+    let p1 = document.createElement('p');
+    let p2 = document.createElement('p');
     let portrait = document.createElement('img');
   
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${prophet.name} ${prophet.lastname};`
+    h2.textContent = `${prophet.name} ${prophet.lastname}`
+    p1.textContent = `Birthday: ${prophet.birthdate}`
+    p2.textContent = `Birthplace: ${prophet.birthplace}`
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait .setAttribute('src', prophet.imageurl);
@@ -29,8 +42,31 @@ function displayProphets(prophet) {
   
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
+    card.appendChild(p1);
+    card.appendChild(p2);
     card.appendChild(portrait);
   
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
   }
+
+
+
+  //Footer Meta scripts
+//Will change content depending on the size of the window
+let last_mod = document.lastModified
+let footer_text = `&copy; ${d.getFullYear()} Ozamiz Chamber | <strong>Jeevee Greg Azores</strong> | WDD 230 Project | Last Modification: ${last_mod}`
+let footer_text2 = `&copy; ${d.getFullYear()} Ozamiz Chamber<br><strong>Jeevee Greg Azores</strong><br>WDD 230 Project<br>Last Modification: ${last_mod}`
+let footer_copyright = document.getElementById("footer-copyright")
+footer_copyright.innerHTML = footer_text
+let size = 1080
+
+window.addEventListener("resize", checkSize)
+function checkSize() {
+    size = footer_copyright.offsetWidth 
+    if (size < 500)  {
+        footer_copyright.innerHTML = footer_text2
+    } else {
+        footer_copyright.innerHTML = footer_text
+    }
+}

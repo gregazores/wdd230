b46e5c93c5f7414fe0ac72af9b0c07d904ba9caf
gregclar2019 <daddygreggy09@gmail.com>
2022-12-16 02:24:04 -0500

    create a drink script

--- bountiful/scripts/drink.js
+++ bountiful/scripts/drink.js
@@ -1,5 +1,6 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/fruit.json'
 const fruitData = []
+let orderHTML = ''
 
 async function apiFetch() {
     try {
@@ -52,7 +53,122 @@ function fillSelect(datas) {
 
 }
 
-function createOrder() {
+function createOrder(event) {
+    event.preventDefault()
+    const fname = document.querySelector('#fname input').value
+    const lname = document.querySelector('#lname input').value
+    const email = document.querySelector('#email input').value
+    const phone = document.querySelector('#phone-num input').value
+    const fruit1 = document.querySelector('#fruit1 select').value
+    const fruit2 = document.querySelector('#fruit2 select').value
+    const fruit3 = document.querySelector('#fruit3 select').value
+    let msg = document.querySelector('#msg textarea').value
+    console.log(msg.length)
+    if (msg.length == 0) {
+        msg = 'No special message specified.'
+    }
+    
+    let carb = 0
+    let protein = 0
+    let fat = 0
+    let calories = 0
+    let sugar = 0
+
+    const fruitArray = fruitData.filter((fruit) => {
+        console.log(fruit.name)
+        if ([fruit1, fruit2, fruit3].includes(fruit.name)) {
+            return fruit
+        }
+    })
+
+    console.log(fruitArray)
+
+    fruitArray.forEach((fruit) => {
+        carb += fruit.nutritions.carbohydrates
+        protein += fruit.nutritions.protein
+        fat += fruit.nutritions.fat
+        calories += fruit.nutritions.calories
+        sugar += fruit.nutritions.sugar
+    })
+
+
+     orderHTML = `
+
+        <div class="order-header">
+        <h2>Hi, ${fname}</h2>
+        <p><em>Thank you for your order</em></p>
+        </div>
+        <div class="customer-info">
+        <h2>Customer Information</h2>
+        <p class="full-name">${fname} ${lname}</p>
+        <p>${email}</p>
+        <p>${phone}</p>
+        </div>
+        <div class="order-details">
+        <h2>Order Details</h2>
+        <p class="date"><em>December 15, 2022</em></p>
+        <div class="selected-fruits">
+            <h3>Selected Fruits</h3>
+            <ul>
+                <li>${fruit1}</li>
+                <li>${fruit2}</li>
+                <li>${fruit3}</li>
+            </ul>
+        </div>
+
+        <div class="nutrition-facts">
+            <h3>Nutrition Facts</h3>
+            <ul>
+                <li>Carbohydrates: ${carb.toFixed(2)}g</li>
+                <li>Protein: ${protein.toFixed(2)}g</li>
+                <li>Fat: ${fat.toFixed(2)}g</li>
+                <li>Sugar: ${sugar.toFixed(2)}g</li>
+                <li>Calories: ${calories.toFixed(2)}g</li>
+            </ul>
+        </div>
+
+        <h3>Special Instructions</h3>
+        <p>${msg}</p>
+
+        </div>
+
+        `
+        //testing()
+        document.querySelector('.fresh-page .create-specialty-drink.hero-call-to-action .hero-call-left .order-confirmation').innerHTML = orderHTML
     
 }
+
+
+
+
+
+/*
+let x = 1
+
+window.onload = function() {testing()}
+
+function testing() {
+
+if (x == 1) {
+    document.querySelector('.fresh-page .create-specialty-drink.hero-call-to-action .hero-call-left .order-confirmation').innerHTML = orderHTML
+
+} 
+
+if (x == 2) {
+    
+    document.querySelector('.fresh-page .create-specialty-drink.hero-call-to-action .hero-call-left .order-confirmation').innerHTML = `
+    <div class="order-header">
+        <h2>Hi, Greg</h2>
+        <p><em>You have not placed a new order yet!</em></p>
+    </div>
+    
+    `
+}
+
+}
+
+*/
+
+
+//document.querySelector('.fresh-page .create-specialty-drink.hero-call-to-action .hero-call-left .order-confirmation').innerHTML
 
